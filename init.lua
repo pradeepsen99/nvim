7edc8d9a92a92b03086e9008ff45ce6129fcf620
Pradeep Senthil <pradeepsen99@gmail.com>
2024-03-09 22:12:36 -0600

    chore: added git submodules for packages

---
+++ init.lua
@@ -0,0 +1,77 @@
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
+})
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    find_files = {
+	themes = "get_dropdown"
+    }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
+-- Enable nvim-treesitter and configure it for TypeScript
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {'typescript'}, -- Install the parser
+  highlight = {
+    enable = true,              -- Enable syntax highlighting
+    disable = {},               -- Disable syntax highlighting
+  },
+}
+
+--require'lspconfig'.tsserver.setup {}
+
+-- setup must be called before loading
+vim.cmd("colorscheme kanagawa-wave")
+
+-- Main settings
+vim.wo.relativenumber = true
+vim.opt.completeopt = "noinsert,menuone,noselect"
+vim.opt.showtabline=2
